fb61c57828b2db2c886a35e02abe64283c297ee7
mikeythacker <mikeythacker@icloud.com>
2025-03-03 20:29:39 +0000

    Complete in maybe log time. unsure if faster as i still think it takes mn log n time

--- src/Solution.java
+++ src/Solution.java
@@ -15,32 +15,37 @@ public class Solution {
 
     public static int matrixMaxValue(int[][] matrix) {
         // Returns the maximum value in the matrix
-
-        int maxEntry = matrix[0][0];
-
-        int lastMaxColumn = 0;
-
-        for (int i = 0; i < matrix.length; i++) {
-            int maxInRow = maxIndex(matrix[i], lastMaxColumn, matrix[i].length - 1);
-            lastMaxColumn = maxInRow;
-            maxEntry = Math.max(maxEntry, matrix[i][maxInRow]);
-        }
-        return maxEntry;
+        return blockMaxValue(matrix, 0, 0, matrix.length - 1, matrix[0].length - 1);
     }
 
     public static int blockMaxValue(int[][] matrix, int startRow, int startCol, int
             endRow, int endCol) {
         // Returns the maximum entry in the matrix between columns startCol, endCol inclusive and between rows startRow,
         // endRow inclusive
-        int maxEntry = matrix[startRow][startCol];
-        int lastMaxColumn = startCol;
 
-        for (int i = startRow; i <= endRow; i++) {
-            int maxInRow = maxIndex(matrix[i], lastMaxColumn, endCol);
-            lastMaxColumn = maxInRow;
-            maxEntry = Math.max(maxEntry, matrix[i][maxInRow]);
+        // Check for base case
+        if (startRow == endRow) {
+            return matrix[startRow][maxIndex(matrix[startRow], startCol, endCol)];
         }
-        return maxEntry;
+
+        // Get the largest element in middle row
+        int middleRow = startRow + (endRow - startRow) / 2;
+        int maxEntryIndex = maxIndex(matrix[middleRow], startCol, endCol);
+        int maxEntry = matrix[middleRow][maxEntryIndex];
+
+
+        int topHalfMax = matrix[startRow][startCol];
+        int bottomHalfMax = matrix[startRow][startCol];
+
+        // Get the largest element in all rows above and below following rule 2
+        if (middleRow > startRow) {
+            topHalfMax = blockMaxValue(matrix, startRow, startCol, middleRow - 1, maxEntryIndex);
+        }
+        if (middleRow < endRow) {
+            bottomHalfMax = blockMaxValue(matrix, middleRow + 1, maxEntryIndex, endRow, endCol);
+        }
+
+
+        return Math.max(maxEntry, Math.max(topHalfMax, bottomHalfMax));
     }
 }
-
